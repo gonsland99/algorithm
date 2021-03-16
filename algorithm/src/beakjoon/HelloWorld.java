343a88a14c83acda0316f4b6e1caa30d531f6cea
kimbyunggon <byunggon.kim.dev@gmail.com>
2021-03-16 23:37:21 +0900

    백준

---
+++ algorithm/src/beakjoon/HelloWorld.java
@@ -0,0 +1,26 @@
+package beakjoon;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+	}
+}
+/*
+import java.util.*;
+public class Main{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+}
+*/
