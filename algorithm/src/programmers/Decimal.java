68411eaf1769e831035ed935793fba1d86dc8eeb
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-03-21 00:05:47 +0900

    소수찾기

--- algorithm/src/programmers/Decimal.java
+++ algorithm/src/programmers/Decimal.java
@@ -15,13 +15,14 @@ public class Decimal {
 		int answer = 0;
 		boolean chk;
 
-		for (int i=2; i<=n; i++) {
+		for(int i=2; i<=n; i++) {
 			chk = true;
-			for (int j=i; j<i; j++)
-				if (i%j == 0) {
+			for(int j=2; j<i; j++) {
+				if(i%j == 0) {
 					chk = false;
 					break;
 				}
+			}
 			if(chk)	answer++;
 		}
 		return answer;
