0f9a30795571c0b55a38ff8b3b9fab9951ab2ada
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-05-11 14:09:46 +0900

    선형검색

--- algorithm/src/algorithm/search/SeqSearch.java
+++ algorithm/src/algorithm/search/SeqSearch.java
@@ -2,66 +2,44 @@ package algorithm.search;
 
 import java.util.Scanner;
 
+//선형 검색: 순서대로
 public class SeqSearch {
-	//�����˻�: ������� �˻�
+	static int seqSearch(int[] a, int n, int key) {
+		//while문
+//		int i = 0;
+//		while(true) {
+//			if(i == n)	
+//				return -1;	//검색실패
+//			if(a[i] == key) 
+//				return i;	//검색성공
+//			i++;
+//		}
+		//for문
+		for(int i=0; i<n; i++) {
+			if(a[i] == key)
+				return i;
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("�����: ");		//��Ҽ� num ����
+		System.out.print("요소수: ");
 		int num = sc.nextInt();
-		//int [] x = new int[num];
-		int [] x = new int[num+1];
+		int[] x = new int[num];
 		
-		for(int i=0; i<num; i++) {		//��� �� �Է�
+		for(int i=0; i<num; i++) {
 			System.out.print("x["+i+"]: ");
 			x[i] = sc.nextInt();
 		}
 		
-		System.out.print("�˻���: ");		//ã�� �� �Է�
+		System.out.print("찾을숫자: ");
 		int ky = sc.nextInt();
 		
-		int idx = seqSearch(x, num, ky);	//�˻� �� ��� ���
+		int idx = seqSearch(x, num, ky);
 		if(idx == -1)
-			System.out.println(ky+"��(��) ã�� �� �����ϴ�.");
+			System.out.println(ky+"은(는) 없습니다.");
 		else
-			System.out.println(ky+"��(��) x["+idx+"]�� �ֽ��ϴ�.");
-	}
-	
-	//key���� ã�� �޼���
-	static int seqSearch(int[] a,int n, int key) {
-		/* 	���ʹ�
-		 	: �迭 �� ���� �� �˻� �� ���ǹ��� �ϳ� �� ���(����� �� �߻�)
-		 	�˻����� �迭 ���� key���� �߰��Ͽ� ���ǹ��� ����(����� ����)	
-		*/
-		/*	���⵵
-			: �ݺ����� ���� ������ �����ϴ� ���α׷��� ���⵵�� ���ٰ� ǥ��
-		*/
-		a[n] = key;
-		int i = 0;
-		while(true) {
-			if(a[i] == key)
-				break;
-			i++;
-		}
-		return i==n ? -1:i;
-		
-		/*
-		//while���� �̿��� �˻��Լ�
-		int i = 0;
-		while(true) {
-			if(i == n)	//�˻� ����
-				return -1;
-			if(a[i] == key) //key���� �ִٸ� i��° ���� ��ȯ
-				return i;
-			i++;
-		}
-	
-		//for���� �̿��� �˻��Լ�
-		for(int i=0; i<n; i++)
-			if(a[i] == key)
-				return i;
-		return -1;
-		*/
-		
+			System.out.println(ky+"은(는) x["+idx+"]에 있습니다.");
 	}
 }
