77fecf800d91f7831f869052415402b62aac859c
kimbyunggon <62224768+kimbyunggon@users.noreply.github.com>
2021-05-11 14:09:53 +0900

    이진검색

--- algorithm/src/algorithm/search/BinSearch.java
+++ algorithm/src/algorithm/search/BinSearch.java
@@ -2,41 +2,14 @@ package algorithm.search;
 
 import java.util.Scanner;
 
+//이진트리검색
 public class BinSearch {
-	//�����˻�: �߰������� �������� �ݾ� �Ұ��Ͽ� ���� ã�Ƴ���
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("��Ҽ�: ");
-		int num = sc.nextInt();
-		int[] x = new int[num];
-		
-		System.out.println("������������ �Է��ϼ���.");
-		System.out.print("x[0]: ");
-		x[0]=sc.nextInt();
-		for(int i=1; i<num; i++) {
-			do {
-				System.out.print("x["+i+"]: ");
-				x[i]=sc.nextInt();
-			}while(x[i] < x[i-1]);
-		}
-		
-		System.out.print("�˻���: ");
-		int ky = sc.nextInt();
-		
-		int idx = binSearch(x, num, ky);
-		if(idx == -1)
-			System.out.println(ky+"��(��) ã�� �� �����ϴ�.");
-		else
-			System.out.println(ky+"��(��) x["+idx+"]�� �ֽ��ϴ�.");
-	}
-	
 	static int binSearch(int[] a, int n, int key) {
-		int pl = 0;	//�˻����� ù�ε���
-		int pr = n-1;	//�˻����� ���ε���
+		int pl = 0;		//첫번째 인덱스
+		int pr = n-1;	//마지막 인덱스
 		
 		do {
-			int pc = (pl+pr)/2;	//�߾ӿ�� �ε���
+			int pc = (pl+pr)/2;	//중간 인덱스
 			if(a[pc] == key)
 				return pc;
 			else if(a[pc] < key)
@@ -46,5 +19,33 @@ public class BinSearch {
 		}while(pl <= pr);
 		return -1;
 	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("요소수: ");
+		int num = sc.nextInt();
+		int[] x = new int[num];
+		
+		System.out.println("오름차순으로 입력할것!");
+		System.out.print("x[0]: ");
+		x[0]=sc.nextInt();
+		for(int i=1; i<num; i++) {
+			do {
+				System.out.print("x["+i+"]: ");
+				x[i]=sc.nextInt();
+			}while(x[i] < x[i-1]);
+		}
+		
+		System.out.print("찾을숫자: ");
+		int ky = sc.nextInt();
+		
+		int idx = binSearch(x, num, ky);
+		if(idx == -1)
+			System.out.println(ky+"이 없습니다.");
+		else
+			System.out.println(ky+"은 x["+idx+"]에 있습니다.");
+	}
+	
+
 	
 }
